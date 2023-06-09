4b8440dd61fffbe6c946886d6ca7cf67c34dc969
Daria Shpak <darya.shpak@gmail.com>
2023-06-09 13:54:39 +0300

    Creates fetch-trailer function

---
+++ src/js/fetches/fetch-trendings.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const API_KEY = '3e1aa277fd6b8a3cd0a3e29dfce20a5c';
+
+const fetchTrendingMovies = async () => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`
+  );
+  return response.data.results;
+};
+
+export { fetchTrendingMovies };
