5897d0176a93a07acc72c30675c39fd5d822490d
Shprinher <119606175+Shprinher@users.noreply.github.com>
2023-06-10 22:42:43 +0300

    part of css2

---
+++ src/js/search-block.js
@@ -0,0 +1,26 @@
+import flatpickr from "flatpickr";
+
+
+
+//   flatpickr("#yearOfFilm", {
+//     dateFormat: "Y",
+//     minDate: "2015",
+//     maxDate: "2023",
+//     static: true,
+//     onReady: function(selectedDates, dateStr, instance) {
+//       const dropdown = instance.calendarContainer;
+//       dropdown.style.display = "none";
+//     },
+//     onOpen: function(selectedDates, dateStr, instance) {
+//       const dropdown = instance.calendarContainer;
+//       dropdown.style.display = "block";
+//     },
+//     onClose: function(selectedDates, dateStr, instance) {
+//       const dropdown = instance.calendarContainer;
+//       dropdown.style.display = "none";
+//     },
+//     onChange: function(selectedDates, dateStr, instance) {
+//       const yearInput = document.getElementById('yearOfFilm');
+//       yearInput.value = dateStr;
+//     }
+//   });
