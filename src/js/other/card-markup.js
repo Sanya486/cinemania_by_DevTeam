717ff7f37b5ec5e17b45c801155cea7b0d030926
demonikos <119507967+demonikos@users.noreply.github.com>
2023-06-10 22:42:51 +0300

    fixed bugs on card-markup

--- src/js/other/card-markup.js
+++ src/js/other/card-markup.js
@@ -1,24 +1,52 @@
-import { fetchMovieDetails } from '../fetches/fetch-movie-details.js';
+// import axios from 'axios';
 
-fetchMovieDetails(54235)
+import starFull from '../images/icons/stars/star-full.svg';
+import starHalf from '../images/icons/stars/star-half.svg';
+import starEmpty from '../images/icons/stars/star-empty.svg';
+
+import { fetchMovieDetails } from './fetches/fetch-movie-details.js';
+
+/* Function expects film's ID and returns finished markup of film's card */
+
+const cardMarkup = async (movieId) => {
+
+const details = await fetchMovieDetails(movieId)
   .then(responce => {
-    console.log(responce);
-    let fixedRating = Math.round(responce.voteAverage);
-    let array = ['', '', '', '', ''];
+    // console.log(responce);
 
-    const valueChoose = {
-      full: './images/icons/stars/star-full.svg',
-      half: './images/icons/stars/star-half.svg',
-      empty: './images/icons/stars/star-empty.svg',
+    const fetchData = {
+      id: responce.id,
+      smallPoster: responce.smallPoster,
+      title: responce.title,
+      genres: responce.genres,
+      year: responce.date.slice(0, 4),
+      rate: Math.round(responce.voteAverage),
     };
 
-    const newArray = array.map(star => {
-      if (fixedRating !== 0) {
-        if (fixedRating > 2) {
-          fixedRating -= 2;
+    // console.log (fetchData);
+
+    let array = ['', '', '', '', ''];
+
+    // const valueChoose = {
+    //   full: 'star-full.svg',
+    //   half: 'star-half.svg',
+    //   empty: 'star-empty.svg',
+    // };
+
+    const valueChoose = {
+      full: starFull,
+      half: starHalf,
+      empty: starEmpty,
+    };
+
+
+    const newArray = array.map(() => {
+      if (fetchData.rate !== 0) {
+        if (fetchData.rate > 2) {
+          fetchData.rate -= 2;
           return valueChoose.full;
-        } else if (fixedRating == 1) {
-          fixedRating -= 1;
+        } else if (fetchData.rate == 1) {
+          fetchData.rate -= 1;
           return valueChoose.half;
         }
       } else {
@@ -30,3 +58,13 @@ fetchMovieDetails(54235)
     return newArray;
   })
   .catch(error => console.log(error));
+
+
+return details;
+};
+
+// cardMarkup(12573);
+// console.log(cardMarkup(12573))
+
+export {cardMarkup};
+
---
+++ src/js/test-card-markup.js
@@ -0,0 +1,35 @@
+// import { fetchMovieDetails } from './fetches/fetch-movie-details.js';
+import { cardMarkup } from './card-markup';
+const testForm = document.querySelector('.test-form');
+const testInput = document.querySelector('.test-input');
+const testButton = document.querySelector('.test-button');
+const testCard = document.querySelector('.test-card');
+
+testForm.addEventListener('submit', testOnSubmit);
+
+function testOnSubmit(event) {
+    event.preventDefault();
+    const typedID = +testInput.value;
+    console.log(typedID);
+
+    cardMarkup(typedID)
+
+
+    // console.log(typeof(typedID));
+
+    cardMarkup(typedID).then(response => {
+        const newMarkup = response;
+
+        console.log(newMarkup);
+
+        testCard.insertAdjacentHTML('beforeend', newMarkup);
+    })
+    .catch(error => console.log(error));
+
+    // console.dir(cardMarkup(typedID));
+
+    // const newMarkup = cardMarkup(typedID);
+    // console.log(newMarkup);
+
+    // testCard.insertAdjacentHTML('beforeend', newMarkup);
+}
