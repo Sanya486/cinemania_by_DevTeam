dac189641a545da9b8f01eb56b981f611c2629f9
demonikos <119507967+demonikos@users.noreply.github.com>
2023-06-11 00:20:52 +0300

    fourth try

--- src/js/other/test-card-markup.js
+++ src/js/other/test-card-markup.js
@@ -1,8 +1,6 @@
-// import { fetchMovieDetails } from './fetches/fetch-movie-details.js';
 import { cardMarkup } from './card-markup';
 const testForm = document.querySelector('.test-form');
 const testInput = document.querySelector('.test-input');
-// const testButton = document.querySelector('.test-button');
 const testCard = document.querySelector('.test-card');
 
 testForm.addEventListener('submit', testOnSubmit);
@@ -14,9 +12,6 @@ function testOnSubmit(event) {
 
     cardMarkup(typedID)
 
-
-    // console.log(typeof(typedID));
-
     cardMarkup(typedID).then(response => {
         const newMarkup = response;
 
@@ -25,11 +20,4 @@ function testOnSubmit(event) {
         testCard.insertAdjacentHTML('beforeend', newMarkup);
     })
     .catch(error => console.log(error));
-
-    // console.dir(cardMarkup(typedID));
-
-    // const newMarkup = cardMarkup(typedID);
-    // console.log(newMarkup);
-
-    // testCard.insertAdjacentHTML('beforeend', newMarkup);
 }
