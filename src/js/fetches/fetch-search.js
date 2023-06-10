9aad46aa5761d6482041ee31ba92f11a8881c293
Shprinher <119606175+Shprinher@users.noreply.github.com>
2023-06-10 16:04:06 +0300

    uninstall flatpickr and make somt changes in css

--- src/js/fetches/fetch-search.js
+++ src/js/fetches/fetch-search.js
@@ -1,48 +1,59 @@
 import flatpickr from "flatpickr";
 
-
 document.getElementById('search-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const searchQuery = document.getElementById('searchQuery').value;
-    const yearOfFilm = document.getElementById('yearOfFilm').value;
+  const searchQuery = document.getElementById('searchQuery').value;
+  const yearOfFilm = document.getElementById('yearOfFilm').value;
 
-    makeBackendRequest(searchQuery, yearOfFilm);
-  });
+  makeBackendRequest(searchQuery, yearOfFilm);
+});
 
-  function makeBackendRequest(query, year) {
-    const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=3e1aa277fd6b8a3cd0a3e29dfce20a5c&query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1`;
+function makeBackendRequest(query, year) {
+  const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=3e1aa277fd6b8a3cd0a3e29dfce20a5c&query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1&year=${year}`;
 
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        console.log('Backend response:', data);
-        displaySearchResults(data.results);
-      })
-      .catch(error => {
-        console.log('Error:', error);
-    
-      });
-  }
-
-  function displaySearchResults(results) {
-    const cardList = document.querySelector('.card-list-search-result');
-
-
-    cardList.innerHTML = '';
-
-    results.forEach(result => {
-      const cardItem = document.createElement('li');
-      cardItem.classList.add('card-item-search-result');
-      cardItem.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500${result.poster_path}')`;
-      cardList.appendChild(cardItem);
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log('Backend response:', data);
+      displaySearchResults(data.results);
+    })
+    .catch(error => {
+      console.log('Error:', error);
     });
-  }
+}
+
+function displaySearchResults(results) {
+  const cardList = document.querySelector('.card-list-search-result');
+  cardList.innerHTML = '';
+
+  results.forEach(result => {
+    const cardItem = document.createElement('li');
+    cardItem.classList.add('card-item-search-result');
+    cardItem.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500${result.poster_path}')`;
+    cardList.appendChild(cardItem);
+  });
+}
 
 flatpickr("#yearOfFilm", {
-    dateFormat: "Y",
-    minDate: "2015",
-    maxDate: "2023"
-  })
-
-  
+  dateFormat: "Y",
+  minDate: "2015",
+  maxDate: "2023",
+  static: true,
+  onReady: function(selectedDates, dateStr, instance) {
+    const dropdown = instance.calendarContainer;
+    dropdown.style.display = "none";
+  },
+  onOpen: function(selectedDates, dateStr, instance) {
+    const dropdown = instance.calendarContainer;
+    dropdown.style.display = "block";
+  },
+  onClose: function(selectedDates, dateStr, instance) {
+    const dropdown = instance.calendarContainer;
+    dropdown.style.display = "none";
+  },
+  onChange: function(selectedDates, dateStr, instance) {
+    const yearInput = document.getElementById('yearOfFilm');
+    yearInput.value = dateStr;
+  }
+});
