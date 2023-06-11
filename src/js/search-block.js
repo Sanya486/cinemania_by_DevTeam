97039c04b927e19622bdbd6fb5b292542d2ae528
Shprinher <119606175+Shprinher@users.noreply.github.com>
2023-06-11 19:31:55 +0300

    make btn close on svg

--- src/js/search-block.js
+++ src/js/search-block.js
@@ -17,3 +17,39 @@
 //       console.log("Обраний рік:", dateStr);
 //     }
 //   });
+
+// const inputFilmName = document.querySelector('.input-film-name-search-form');
+// const inputCloseSvg = document.querySelector('.input-close-svg');
+// const searchForm = document.querySelector('.film-name-search-form');
+
+// // При натисканні на інпут
+// inputFilmName.addEventListener('input', () => {
+//   const inputValue = inputFilmName.value.trim();
+
+//   if (inputValue !== '') {
+//     inputCloseSvg.classList.remove('hidden');
+//   } else {
+//     inputCloseSvg.classList.add('hidden');
+//   }
+// });
+
+// // При натисканні на кнопку з SVG
+// inputCloseSvg.addEventListener('click', () => {
+//   inputFilmName.value = '';
+//   inputCloseSvg.classList.add('hidden');
+// });
+
+// // При натисканні на баттон форми
+// searchForm.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   // Отримати значення з інпута
+//   const searchQuery = inputFilmName.value.trim();
+
+//   // Виконати необхідні дії зі значенням пошукового запиту, наприклад, відправити на сервер або виконати пошук
+//   // Ваш код тут
+
+//   // Очистити значення інпута
+//   inputFilmName.value = '';
+//   inputCloseSvg.classList.add('hidden');
+// });
