2f38a82a7da57aae719a47eff002cce3158d2b3e
Sanya486 <120394126+Sanya486@users.noreply.github.com>
2023-06-12 00:38:14 +0300

    switcher

--- src/js/other/scroll-up.js
+++ src/js/other/scroll-up.js
@@ -3,11 +3,15 @@ const backToTopButton = document.querySelector('#back-to-top');
 window.addEventListener('scroll', () => {
   if (window.scrollY > 100) {
     backToTopButton.style.display = 'block';
-  } else {
+    backToTopButton.addEventListener('click', windowScroll);
+    }
+   else {
     backToTopButton.style.display = 'none';
+    backToTopButton.removeEventListener('click', windowScroll)
   }
 });
 
-backToTopButton.addEventListener('click', () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+function windowScroll () {
+ window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+  
