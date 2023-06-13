a1a516d8e0d402eff77018712d57e22d2277eed8
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-06-13 14:42:29 +0300

    add hero preloader
    Саша как будешь смотреть на этот пул реквест давай созвонимся. У меня есть камент на эту тему.

--- src/js/other/hero-markup.js
+++ src/js/other/hero-markup.js
@@ -1,63 +1,83 @@
 import axios from 'axios';
 import { fetchMovieDetails } from '../fetches/fetch-movie-details';
-import {rateArray} from './rate-markup';
+import { rateArray } from './rate-markup';
 
 const refs = {
-    heroContainer: document.querySelector('.home-hero > .container'),
-}
+  heroContainer: document.querySelector('.home-hero > .container'),
+};
 
 const API_KEY = '3e1aa277fd6b8a3cd0a3e29dfce20a5c';
 const timeWindow = 'day';
 
-const fetchTrendingDayMovies = async () => {
-    const response = await axios.get(
-    `https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=${API_KEY}`
-    );
+//////---------------------------
+function createPreloader() {
+  const preloader = document.createElement('div');
+  preloader.classList.add('preloader');
+  return preloader;
+}
 
-    const data = response.data;
-    const movieIds = data.results.map(movie => movie.id);
-    
-    return movieIds;
+function showPreloader() {
+  const preloader = createPreloader();
+  refs.heroContainer.prepend(preloader);
+}
+
+function hidePreloader() {
+  const preloader = document.querySelector('.preloader');
+  if (preloader) {
+    preloader.remove();
+  }
+}
+
+//////---------------------------
+
+const fetchTrendingDayMovies = async () => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=${API_KEY}`
+  );
+
+  const data = response.data;
+  const movieIds = data.results.map(movie => movie.id);
+
+  return movieIds;
 };
 
 window.addEventListener('load', onLoadHero);
 
 async function onLoadHero() {
-    try {
-        const films = await fetchTrendingDayMovies();
-        makeMarkUpRandomFilm(films);
-        
-    } catch(error) {
-        throw new Error(error.message);
-    }
+  try {
+    const films = await fetchTrendingDayMovies();
+    makeMarkUpRandomFilm(films);
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
 async function makeMarkUpRandomFilm(films) {
-    try {
-        const randomIndex = Math.floor(Math.random() * films.length);
-        const randomId = films[randomIndex];
-        const filmInfo = await fetchMovieDetails(randomId);
-        markUpApiHero(filmInfo);
-    } catch(error) {
-        throw new Error(error.message);
-    }
+  try {
+    const randomIndex = Math.floor(Math.random() * films.length);
+    const randomId = films[randomIndex];
+    const filmInfo = await fetchMovieDetails(randomId);
+    markUpApiHero(filmInfo);
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
 function markUpApiHero(filmInfo) {
-    const { title, overview, smallPoster, poster, voteAverage, id } = filmInfo;
-    const roundedRating = Math.round(voteAverage);
+  const { title, overview, smallPoster, poster, voteAverage, id } = filmInfo;
+  const roundedRating = Math.round(voteAverage);
 
-    const quaryRate = rateArray(roundedRating);
-    const rateMarkup = quaryRate
-          .map(
-            elem =>
-              `<li class="rating-item">
+  const quaryRate = rateArray(roundedRating);
+  const rateMarkup = quaryRate
+    .map(
+      elem =>
+        `<li class="rating-item">
                <img class="rating-icon" src="${elem}" alt="">
                </li>`
-          )
-          .join('');
-    
-    const markUp = `<div class="home-hero-api-movie">
+    )
+    .join('');
+
+  const markUp = `<div class="home-hero-api-movie">
       <div class="home-hero-api-movie-con"><h2 class="home-hero-api-movie-name">${title}</h2>
     <div class="home-hero-api-movie-rating">
       <ul class="film-rating-hero">
@@ -88,6 +108,7 @@ function markUpApiHero(filmInfo) {
     </div>
     </div>`;
 
-    refs.heroContainer.innerHTML = markUp;
-    
+  // refs.heroContainer.innerHTML = markUp;
+  refs.heroContainer.insertAdjacentHTML('beforeend', markUp);
+  hidePreloader();
 }
