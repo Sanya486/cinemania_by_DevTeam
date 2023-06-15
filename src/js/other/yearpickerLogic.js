3a8b445e28a2cb1d1d5d48a2c74366e7d1e26f22
Sanya486 <120394126+Sanya486@users.noreply.github.com>
2023-06-15 03:23:43 +0300

    fix years

--- src/js/other/yearpickerLogic.js
+++ src/js/other/yearpickerLogic.js
@@ -2,5 +2,9 @@ import './jquery'
 import './yearpicker'
 import '../../css/utils/yearpicker.css'
 
+let today = new Date(); 
+let year = today.getFullYear();
 
-$(".yearpicker").yearpicker()
+$(".yearpicker").yearpicker({
+    endYear: year,
+})
